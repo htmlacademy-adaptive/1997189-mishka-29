16cb1d0d0b4e03b09efc8e5d1e33c2b79a34ad74
Vera Pervushina <vera.pervushina2608@gmail.com>
2023-11-05 16:33:14 +0700

    menu

--- source/js/script.js
+++ source/js/script.js
@@ -3,6 +3,6 @@ const menuButton = document.querySelector('.toggle');
 
 nav.classList.add('nav--closed');
 menuButton.addEventListener('click',() => {
-  nav.classList.toggle('nav--closed');
   nav.classList.toggle('nav--opened');
+  nav.classList.toggle('nav--closed');
   });
